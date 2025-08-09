264418139037f474bee3422715f01a93c6dda896
founder222 <tech344445@gmail.com>
2025-08-10 01:59:02 +0300

    Update promotions.ts

--- src/constants/promotions.ts
+++ src/constants/promotions.ts
@@ -3,6 +3,6 @@ import { PromotionType } from '@prisma/client'
 export const GET_50_WALLETS_PROMOTION = {
   price: 0.1,
   type: PromotionType.UPGRADE_TO_50_WALLETS,
-  description: 'Track up to 50 wallets life-time',
+  description: 'Отслеживайте до 50 кошельков навсегда',
   isActive: true,
 }
