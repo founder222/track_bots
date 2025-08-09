f6de7059aa667a1d81efcb6836e580dbec8f8e31
founder222 <tech344445@gmail.com>
2025-08-10 01:27:00 +0300

    Update wallet-messages.ts

--- src/bot/messages/wallet-messages.ts
+++ src/bot/messages/wallet-messages.ts
@@ -8,21 +8,21 @@ export class WalletMessages {
   }
 
   static addWalletMessage: string = `
-🐱 Ok, just send me a wallet address to track:
+🐱 Хорошо, просто отправьте мне адрес кошелька для отслеживания:
 
-You can also give that wallet a name by following the address with the desired name, or add multiple wallets at once by sending them each on a new line for example: 
+Вы также можете дать кошельку имя, добавив после адреса желаемое имя, или добавить несколько кошельков сразу, отправив их по одному на новой строке, например:
 
-walletAddress1 walletName1
-walletAddress2 walletName2
+адресКошелька1 имяКошелька1
+адресКошелька2 имяКошелька2
 `
 
   static deleteWalletMessage: string = `
-Send me the wallet address you want to remove 🗑️
+Отправьте мне адрес кошелька, который хотите удалить 🗑️
 
-You can also delete multiple wallets at once if you send them each on a new line, for example:
+Вы также можете удалить несколько кошельков сразу, если отправите их по одному на новой строке, например:
 
-walletAddress1
-walletAddress2
+адресКошелька1
+адресКошелька2
 `
 
   public async sendMyWalletMessage(
@@ -31,7 +31,7 @@ walletAddress2
     const solBalance = await this.userBalances.userPersonalSolBalance(wallet.personalWalletPubKey)
 
     const responseText = `
-<b>Your wallet address:</b> 
+<b>Адрес вашего кошелька:</b>
 <code>${wallet && wallet.personalWalletPubKey}</code>
 
 <b>SOL:</b> ${solBalance ? solBalance / 1e9 : 0}
